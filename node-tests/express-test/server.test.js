897e9cff339696fca1972b9a442ee5a2ef599d0e
Wen Jie <wenjie@cavman.com.my>
2018-07-09 18:04:18 +0800

    describe block

--- node-tests/express-test/server.test.js
+++ node-tests/express-test/server.test.js
@@ -2,23 +2,25 @@ const request = require('supertest');
 let app = require('./server').app;
 const expect = require('chai').expect;
 
-it('should return hello world', (done) => {
-    request(app)
-        .get('/')
-        .expect(200)
-        .expect('Hello World')
-        .end(done);
-});
-
-it('should include name', (done) => {
-    request(app)
-        .get('/user')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).to.deep.include({
-                name: 'jie',
-                age: 27
-            });
-        })
-        .end(done);
+describe('Server',() => {
+    it('should return hello world', (done) => {
+        request(app)
+            .get('/')
+            .expect(200)
+            .expect('Hello World')
+            .end(done);
+    });
+    
+    it('should include name', (done) => {
+        request(app)
+            .get('/user')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).to.deep.include({
+                    name: 'jie',
+                    age: 27
+                });
+            })
+            .end(done);
+    });
 });
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,67 +1,74 @@
 const utils = require('./utils');
 const expect = require('chai').expect;
 
-it('should add two numbers', () => {
-    let res = utils.add(33,11);
+describe('Utils', () =>{
 
-    if(res !== 44){
-        throw new Error(`expected 44, but got ${res}`)
-    }
+    describe('Add', () => {
 
-    // expect(res).to.equal(44);
-    // expect(res).to.not.equal(1);
-
-    expect(res).to.be.a('number')
-    expect(res).to.not.be.a('string');
-
-});
-
-it('should square a number', () => {
-    let res = utils.square(3);
-
-    if(res !== 9){
-        throw new Error(`expected 9, but got ${res}`)
-    }
-});
-
-/**
- * deep -> ===
- */
-it('should expect some values', () => {
-    let res = 44;
-    let name = 'jie';
-    let obj = {
-        name:'jie',
-    };
-
-    let obj2 = {
-        a:1,
-        b:2
-    };
-    let arr = [1,2,3];
-
-    // value
-    expect(res).to.deep.equal(44);
-    expect(res).to.not.equal(1);
-
-    // type
-    expect(res).to.deep.be.a('number')
-    expect(res).to.not.be.a('string');
-
-    // object
-    expect(obj).to.deep.equal({
-        name:'jie'
+        it('should add two numbers', () => {
+            let res = utils.add(33,11);
+        
+            if(res !== 44){
+                throw new Error(`expected 44, but got ${res}`)
+            }
+        
+            // expect(res).to.equal(44);
+            // expect(res).to.not.equal(1);
+        
+            expect(res).to.be.a('number')
+            expect(res).to.not.be.a('string');
+        
+        });
+        
     });
-
-    expect(name).to.include('ie');
-    expect(arr).to.deep.include(1);
-    expect(obj2).to.deep.include({a:1});
-});
-
-// async code testing - done
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4,3,(sum) => {
-        expect(sum).to.deep.equal(7).to.deep.to.be.a('number');
-        done();
-    })
+    
+    it('should square a number', () => {
+        let res = utils.square(3);
+    
+        if(res !== 9){
+            throw new Error(`expected 9, but got ${res}`)
+        }
+    });
+    
+    /**
+     * deep -> ===
+     */
+    it('should expect some values', () => {
+        let res = 44;
+        let name = 'jie';
+        let obj = {
+            name:'jie',
+        };
+    
+        let obj2 = {
+            a:1,
+            b:2
+        };
+        let arr = [1,2,3];
+    
+        // value
+        expect(res).to.deep.equal(44);
+        expect(res).to.not.equal(1);
+    
+        // type
+        expect(res).to.deep.be.a('number')
+        expect(res).to.not.be.a('string');
+    
+        // object
+        expect(obj).to.deep.equal({
+            name:'jie'
+        });
+    
+        expect(name).to.include('ie');
+        expect(arr).to.deep.include(1);
+        expect(obj2).to.deep.include({a:1});
+    });
+    
+    // async code testing - done
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(4,3,(sum) => {
+            expect(sum).to.deep.equal(7).to.deep.to.be.a('number');
+            done();
+        })
+    });
 });
