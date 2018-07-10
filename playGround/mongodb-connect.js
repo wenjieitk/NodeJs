3f7b5349158c9c7c02bb27e8718e409182e433a2
Wen Jie <wenjie@cavman.com.my>
2018-07-10 17:58:48 +0800

    connect to mongodb and create db/insert data

---
+++ playGround/mongodb-connect.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,client) =>{ // create TodoApp db
+    if(err) {
+        return console.log('unable to connect to Mongodb Sever');
+    }
+    console.log('connected to mongodb');
+
+    client.db('TodoApp').collection('Todos').insertOne({
+        text:'somthing to do',
+        completed: false
+    },(err,result)=>{
+        if(err){
+            return console.log('unable to insert todo \n',err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2)); // result.ops -> to show the result added
+    });
+
+    client.db('User').collection('Todos').insertOne({
+        text: 'jie',
+        age : false,
+        location: 'KL'
+    },(err,result)=>{
+        if(err){
+            return console.log('unable to insert user \n',err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2)); // result.ops -> to show the result added
+    });
+
+    client.close();
+});
