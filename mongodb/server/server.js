39a74a0d4d61e646fd41fcce1f33af595c6c0606
Wen Jie <wenjie@cavman.com.my>
2018-07-12 16:18:51 +0800

    mongoose validate and default

--- mongodb/server/server.js
+++ mongodb/server/server.js
@@ -5,23 +5,47 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 let Todo = mongoose.model('Todo',{
     text:{
-        type: String
+        type: String,
+        require: true, // cannot be empty
+        minlength: 1, // min length
+        trim:true // remove white space
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt:{
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 
 let newTodo = new Todo({
-    text: 'cook dinner'
+    completed: true
 });
 
 newTodo.save().then((doc)=>{
     console.log('saved todo',doc);
 },(error) =>{
     console.log('unable to save \n ' + error);
+});
+
+let User = mongoose.model('User',{
+    email:{
+        type: String,
+        require: true, // cannot be empty
+        minlength: 1, // min length
+        trim:true // remove white space
+    }
+});
+
+let user = new User({
+    email: "123@qq.com"
+});
+
+user.save().then((doc)=>{
+    console.log('saved user',doc);
+},(error) =>{
+    console.log('user unable to save \n ' + error);
 });
