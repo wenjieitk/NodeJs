1e3ed29dbe31ca86df3110814e4b412a7afb9846
Wen Jie <wenjie@cavman.com.my>
2018-07-13 13:57:31 +0800

    mongoose query with id

---
+++ playGround/mongoose-queries.js
@@ -0,0 +1,22 @@
+const {mongoose} = require('./../mongodb/server/db/mongoose');
+const {Todo} = require('./../mongodb/server/models/todo');
+
+let id = '5b483e01ba36875946125082';
+
+// array format
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('find \n', todos);
+});
+
+// object format
+Todo.findOne({
+    _id: id
+}).then((todos) => {
+    console.log('\nfindOne \n', todos);
+});
+
+Todo.findById(id).then((todos) => {
+    console.log('\nfindById \n', todos);
+});
