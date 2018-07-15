074848165f1aea4e7babb23acd71a75030eb7b0b
Wen Jie <wenjie@cavman.com.my>
2018-07-15 15:41:50 +0800

    setting up mlab and heroku connection

--- mongodb/server/db/mongoose.js
+++ mongodb/server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true } );
+mongoose.connect('mongodb://jiecavman:root123@ds137581.mlab.com:37581/node_1', {useNewUrlParser: true } 
+                 || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true } );
 
 module.exports = {
     mongoose
