35f03120f99bce41a12ffdd8ecbfd0d463d0de1c
Wen Jie <wenjie@cavman.com.my>
2018-07-16 15:33:08 +0800

    configuring project environment -> test/development/production

--- mongodb/server/db/mongoose.js
+++ mongodb/server/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const {port} = require('./../config/env');
+const {env} = require('./../config/env');
 
-const mongoUri = (port === 3000) ? 'mongodb://localhost:27017/TodoApp' : 'mongodb://jiecavman:root123@ds137581.mlab.com:37581/node_1';
+const mongoUri = (env === 'test') ? 'mongodb://localhost:27017/TodoAppTest' 
+                    :(env === 'development') ? 'mongodb://localhost:27017/TodoApp' 
+                        : 'mongodb://jiecavman:root123@ds137581.mlab.com:37581/node_1';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoUri, {useNewUrlParser: true });
