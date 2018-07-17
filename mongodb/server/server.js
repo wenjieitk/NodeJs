5b0a0a825855e433de19ec072f47b89a37440144
Wen Jie <wenjie@cavman.com.my>
2018-07-17 23:22:57 +0800

    simplify code

--- mongodb/server/server.js
+++ mongodb/server/server.js
@@ -130,9 +130,7 @@ app.post('/users', (req, res)=>{
     let body = _.pick(req.body,['email','password']);
     let user = new User(body);
 
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {        
+    user.generateAuthToken().then((token) => {        
         res.header('x-auth', token).send(user);
     }).catch((error) => {
         res.status(400).send(error);
