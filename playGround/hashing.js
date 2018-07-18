07e515093702205b42569ebf2792ef34ce536e43
Wen Jie <wenjie@cavman.com.my>
2018-07-18 21:03:37 +0800

    hashing play around

--- playGround/hashing.js
+++ playGround/hashing.js
@@ -1,11 +1,28 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 10
-};
+// let data = {
+//     id: 10
+// };
 
-let token = jwt.sign(data, '123abc')
-console.log(token);
+// let token = jwt.sign(data, '123abc')
+// console.log(token);
 
-let decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+// let decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
+
+let password = '123abc';
+
+// hasing password with bcrypt
+bcrypt.genSalt(10, (err,salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash); 
+    });
+})
+
+let hashedPassword = '$2a$10$22LEXj4NqNQB5kATbxcLTuOjckcEnymkyCtMzlr5YtOG4sD/0dJqS';
+
+bcrypt.compare(password, hashedPassword, (error,res) =>{
+    console.log(res);
+    
+}); 
