384bec62e464211017a7ac958077c6032e7ceb44
Wen Jie <wenjie@cavman.com.my>
2018-07-23 19:16:08 +0800

    deep into async-await

--- async-await/currency-convert.js
+++ async-await/currency-convert.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 
 const getExchangeRate = async (from, to) => {
   const response = await axios.get('http://data.fixer.io/api/latest?access_key=d32d75de5146611ae7f23de0782ac09b');
-  
   const euro = 1 / response.data.rates[from];
   const rate = euro * response.data.rates[to];
   return rate;
@@ -10,14 +9,16 @@ const getExchangeRate = async (from, to) => {
 
 const getCountries = async (currencyCode) => {
   const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-
   return response.data.map((country) => country.name);
 };
 
-getExchangeRate('USD', 'CAD').then((rate) => {
-  console.log(rate);
-});
+const convertCurrency = async (from, to, amount) => {
+  const rate = await getExchangeRate(from, to);
+  const countries = await getCountries(to);
+  const convertedAmount = (amount * rate).toFixed(2);
+  return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries.join(', ')}`;
+};
 
-getCountries('CAD').then((countries) => {
-  console.log(countries);
+convertCurrency('USD', 'USD', 20).then((message) => {
+  console.log(message);
 });
