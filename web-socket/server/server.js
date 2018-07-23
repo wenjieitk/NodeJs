ba91be969c138de14c2e4066086e7eaf15445043
Wen Jie <wenjie@cavman.com.my>
2018-07-23 17:55:04 +0800

    sokcet chat update

--- web-socket/server/server.js
+++ web-socket/server/server.js
@@ -4,58 +4,77 @@
  * io.emit = emits an envent to EVERY single connection
  * io.on = the socket connection is start
  * socket.broadcast.emit = send to all the connection but me
+ * socket.join = join the specific room
+ * socket.leave = leave the specific room
  */
 
+
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const http = require('http');
 
-const {generateMessage} = require('./utils/message')
-const {port} = require('../config/env');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
 const publicPath = path.join(__dirname,'../public'); // redirect path to specific folder, instead or go through one by one
-let app = express();
-let server = http.createServer(app); // use http server
-let io = socketIO(server);
+const {port} = require('../config/env');
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+var users = new Users();
 
-// to start up the html page
 app.use(express.static(publicPath));
 
-// when server is started
 io.on('connection', (socket) => {
-    console.log('New user connected');
+  console.log('New user connected');
 
-    socket.emit('Welcome Message', generateMessage('Admin', 'Welcome to the chat app'))
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required.');
+    }
 
-    
-  
-    socket.broadcast.emit('Notification', generateMessage('Admin','New User Join'));
-  
-    socket.on('createMessage', (message, callback) => {
-      console.log('createMessage', message);
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
 
-      io.emit('newMessage', generateMessage(message.from, message.text));
-      callback('this string is from callback');
-    });
-
-    socket.on('userLogin', (user) => {
-        io.emit('newMessage', {
-            status: 'Login success',
-            name: user.userName,
-            time: new Date().getTime()
-        });
-      }); 
-  
-    socket.on('disconnect', () => {
-      console.log('User was disconnected');
-    });
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
   });
+
+  socket.on('createMessage', (message, callback) => {
+    var user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
+    callback();
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));  
+    }
+  });
+
+  socket.on('disconnect', () => {
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
+  });
+});
+
 
 /******** Port listening *********/
 server.listen(port,() => {
-    console.log(`Started up at port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
-
-module.exports = {
-    app
-};
--- web-socket/server/utils/message.test.js
+++ web-socket/server/utils/message.test.js
@@ -1,16 +1,27 @@
-const expect = require('chai').expect;
-const { generateMessage } = require('./message')
+var expect = require('expect');
+
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
-    it('should generate correct message object', () => {
-        let from = 'Jen';
-        let text = 'some message';
-        let message = generateMessage(from,text);
+  it('should generate correct message object', () => {
+    var from = 'Jen';
+    var text = 'Some message';
+    var message = generateMessage(from, text);
 
-        expect(message.createdAt).to.be.a('number');
-        expect(message).to.include({
-            from,
-            text
-        });
-    })
-})
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+  });
+});
